57c379d7402afacda60cd0794842f4f2e71366b3
Nataly <ncgarzon88@gmail.com>
2018-07-26 11:28:21 -0400

    updating instant time refresh

--- dist/js/app.js
+++ dist/js/app.js
@@ -8,15 +8,28 @@ var app = new Vue({
 		people: []
 	},
 
+	created: function created() {
+		setInterval(this.updateTimeSinceCheckins, 5 * 1000);
+	},
+	//new vue - create 
+
 	methods: {
+		updateTimeSinceCheckins: function updateTimeSinceCheckins() {
+			this.people.forEach(function (person) {
+				person.timeSinceCheckin = Math.floor((new Date().getTime() - person.timestamp) / 1000 / 60);
+			});
+		},
+
 		addPerson: function addPerson() {
 			console.log("add me!");
+
 			var newPerson = {
 				image: 'https://www.gravatar.com/avatar/' + md5(this.newEmail),
 				name: this.newName,
 				email: this.newEmail,
-				// timestamp: new Date().getTime(),
-				checkin: moment(this.timestamp).format("MMM Do hh:mm:ss a")
+				// checkin: moment(this.timestamp).format("MMM Do hh:mm:ss a"),
+				timestamp: new Date().getTime(),
+				timeSinceCheckin: 0
 				//https://momentjs.com/
 
 				//clear the data from the input field
@@ -28,10 +41,39 @@ var app = new Vue({
 			if (this.people.length > 8) {
 				this.people.shift();
 			}
+
+			// setInterval(function(){
+			// 	newPerson.timeSinceCheckin++
+			// }, 60*1000)
+			//easy time since update 
+
+
+			//after submitting, focus on the first
+			this.$refs.name.focus();
 		}
 
 		// timeSinceLogin: function(){
 
+		// }
+		// function timeSince(date) {
+		// 	var seconds = Math.floor(((new Date().getTime()/1000) - date)),
+		// 	interval = Math.floor(seconds / 31536000);
+
+		// 	if (interval > 1) return interval + "y";
+
+		// 	interval = Math.floor(seconds / 2592000);
+		// 	if (interval > 1) return interval + "m";
+
+		// 	interval = Math.floor(seconds / 86400);
+		// 	if (interval >= 1) return interval + "d";
+
+		// 	interval = Math.floor(seconds / 3600);
+		// 	if (interval >= 1) return interval + "h";
+
+		// 	interval = Math.floor(seconds / 60);
+		// 	if (interval > 1) return interval + " m";
+
+		// 	return Math.floor(seconds) + "s";
 		// }
 	}
 });
@@ -39,5 +81,5 @@ var app = new Vue({
 //for homeworks
 // make it so it just shows the last nine images 
 //format the time
-//bonus make it show the time sense they checked in. So current time minus the time it happened and then recalculating every minute
+//bonus make it show the time since they checked in. So current time minus the time it happened and then recalculating every minute
 //# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -6,15 +6,28 @@ var app = new Vue({
 		people:	[],
 	},
 
-	methods:{
+	created: function(){
+		setInterval(this.updateTimeSinceCheckins, 5*1000)
+	},
+	//new vue - create 
+
+	methods: {
+		updateTimeSinceCheckins: function() {
+			this.people.forEach(function(person) {
+				person.timeSinceCheckin = Math.floor((new Date().getTime() - person.timestamp)/1000/60)
+			})
+		},
+
 		addPerson: function () {
 			console.log("add me!")
+			
 			let newPerson = {
 				image: 'https://www.gravatar.com/avatar/' + md5(this.newEmail),
 				name: this.newName,
 				email: this.newEmail,
-				// timestamp: new Date().getTime(),
-				checkin: moment(this.timestamp).format("MMM Do hh:mm:ss a"),
+				// checkin: moment(this.timestamp).format("MMM Do hh:mm:ss a"),
+				timestamp: new Date().getTime(),
+				timeSinceCheckin: 0
 				//https://momentjs.com/
 
 			}
@@ -27,10 +40,40 @@ var app = new Vue({
 			if (this.people.length > 8){
 				this.people.shift()
 			}
+
+			// setInterval(function(){
+			// 	newPerson.timeSinceCheckin++
+			// }, 60*1000)
+			//easy time since update 
+
+
+			//after submitting, focus on the first
+			this.$refs.name.focus()
+
 		},
 			
 		// timeSinceLogin: function(){
 	
+		// }
+		// function timeSince(date) {
+		// 	var seconds = Math.floor(((new Date().getTime()/1000) - date)),
+		// 	interval = Math.floor(seconds / 31536000);
+		
+		// 	if (interval > 1) return interval + "y";
+		
+		// 	interval = Math.floor(seconds / 2592000);
+		// 	if (interval > 1) return interval + "m";
+		
+		// 	interval = Math.floor(seconds / 86400);
+		// 	if (interval >= 1) return interval + "d";
+		
+		// 	interval = Math.floor(seconds / 3600);
+		// 	if (interval >= 1) return interval + "h";
+		
+		// 	interval = Math.floor(seconds / 60);
+		// 	if (interval > 1) return interval + " m";
+		
+		// 	return Math.floor(seconds) + "s";
 		// }
 	}
 })
@@ -38,4 +81,4 @@ var app = new Vue({
 //for homeworks
 // make it so it just shows the last nine images 
 //format the time
-//bonus make it show the time sense they checked in. So current time minus the time it happened and then recalculating every minute
+//bonus make it show the time since they checked in. So current time minus the time it happened and then recalculating every minute
