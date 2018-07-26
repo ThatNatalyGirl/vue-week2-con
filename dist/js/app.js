7bbd996a50f65978cecea6576a66bc0f844aa585
Nataly <ncgarzon88@gmail.com>
2018-07-25 21:47:01 -0400

    Working on time

--- dist/js/app.js
+++ dist/js/app.js
@@ -15,12 +15,32 @@ var app = new Vue({
 				image: 'https://www.gravatar.com/avatar/' + md5(this.newEmail),
 				name: this.newName,
 				email: this.newEmail,
-				timestamp: new Date().getTime()
+				// timestamp: new Date().getTime(),
+				timestamp: new Date().getTime(),
+				checkin: moment(this.timestamp).format("MMM Do hh:mm:ss a"),
+				lapsedtime: this.sinceTime()
 				//clear the data from the input field
 			};this.newName = '';
 			this.newEmail = '';
 			//adds the new person typed in to the people array
 			this.people.push(newPerson);
+
+			if (this.people.length > 2) {
+				this.people.splice(this.index, 1);
+			}
+			this.setFocus();
+		},
+
+		setFocus: function setFocus() {
+			this.$refs.name.focus();
+		},
+		sinceTime: function sinceTime() {
+			var _this = this;
+
+			this.interval = setInterval(function () {
+				var timestamp = _this.timestamp;
+				console.log('hello');
+			}, 1000);
 		}
 	}
 });
--- src/js/app.js
+++ src/js/app.js
@@ -13,13 +13,31 @@ var app = new Vue({
 				image: 'https://www.gravatar.com/avatar/' + md5(this.newEmail),
 				name: this.newName,
 				email: this.newEmail,
+				// timestamp: new Date().getTime(),
 				timestamp: new Date().getTime(),
+				checkin: moment(this.timestamp).format("MMM Do hh:mm:ss a"),
+				lapsedtime: this.sinceTime()
 			}
 			//clear the data from the input field
 			this.newName = ''
 			this.newEmail = ''
 			//adds the new person typed in to the people array
 			this.people.push(newPerson)
+
+			if (this.people.length > 2){
+				this.people.splice(this.index, 1)
+			}
+			this.setFocus()
+		},
+			
+		setFocus: function(){
+			this.$refs.name.focus();
+		},
+		sinceTime: function(){
+			this.interval = setInterval(() => {
+				let timestamp = this.timestamp
+				console.log(`hello`)
+			}, 1000)
 		}
 	}
 })
